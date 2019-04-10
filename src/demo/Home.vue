8b3214d8afbcff00a49c5268af5fd4bb0a112f4c
zhouyaoyao <624490555@qq.com>
2019-04-10 11:32:52 +0800

    fix:demo

---
+++ src/demo/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <fm-making-form ref="makingForm" upload preview generate-code generate-json class="ssssss">
+    <template slot="action">
+      <a>sss</a>
+    </template>
+  </fm-making-form>
+</template>
+
+<script>
+export default {
+  mounted () {
+  }
+}
+</script>
